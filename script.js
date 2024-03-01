6918e22b3ee3ffa7c0c4319a722f3c915dbddde8
Amit Jha <91418427+jhaami@users.noreply.github.com>
2024-03-02 01:04:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,99 @@
-*{
-    margin: 0;
-    padding: 0;
-}
-body{
-    background-color: #548687;
-    text-align: center;
-}
-.container{
-    /* dynamically height, it means jitana bhi height rahega uska 70vh container ka height rahega */
-    height: 70vh;
-/* box center me lane ke liye  */
-    display: flex;
-    justify-content: center;
-    align-items: center;
+// all boxes access 
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector("#new-btn");
+let msgContainer= document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
 
-}
-.game{
-    height: 60vmin;
-    width: 60vmin;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    gap: 1.5vmin;
-}
-.box{
-    /* game's height 60vmin,aur usme hume 3box dena hain... that's reason 60/3=20; 18vmin diya, aab ek box 18 vmin ka height hoga  */
-    height: 18vmin;
-    width: 18vmin;
-    border-radius: 1rem;
-    border: none;
-    font-size: 8vmin;
-    color: #b0413e;
-    background-color: #f8f8f8;
-}
-#reset-btn{
-    padding: 1rem;
-    font-size: 1.25rem;
-    background-color: #b0413e;
-    color: #fff;
-    border-radius: 1rem;
-    border: none;
-}
-#new-btn{
-    padding: 1rem;
-    font-size: 1.25rem;
-    background-color: #b0413e;
-    color: #fff;
-    border-radius: 1rem;
-    border: none; 
-}
-/* winner ka msg show karane ke liye */
-#msg{
-    color: green;
-    font-size: 6vmin;
-}
-.msg-container{
-    height: 100vmin;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 4rem;
-}
-/* ye isliye ki winning hone ke baad show hona chahiye */
-.hide{
-    display: none;
-}
-/* responsive */
-@media only screen and (max-width: 600px) {
-    .msg-container{
-      height: 100vh;
+
+
+
+// altrnate game turn=X,O,X,O, it means X player ka turn baad O player, X ke baad click O aane chahiye
+let turnO=true; //click O aaega;
+
+// winning pattern store create 2D array aur u r also store string pattern
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [2,4,6],
+    [6,7,8],
+    [3,4,5]
+];
+// boxes jo hain uspar click hone par kux action hona chahiye, eskiye addEventListener use karunga
+// sab boxex me click karwana hain action hona chahiye esliye forEach rakhunga
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("yes working")
+        // aab value ka acces karna hain jiska turn hain 
+        if(turnO){//player O ka turn hain
+            box.innerText="O";
+            turnO=false;//aab O apna turn karliya X ka turn ke liye
+        }
+        else{
+            box.innerText="X";
+            turnO=true;
+        }
+        // yeh esliye select kiya gaya ki upper wale code se O player wale button pe X player click karta tha aur value change ho jata tha 
+        box.disabled=true;//ek bar jab apni turn khel di to aab dubara change nahi kar sakte
+
+        // now, winner check
+        checkWinner();
+
+    })
+});
+//winner ho gaya to box ka disabled karna hai jo phir game na khele
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
     }
-  }
+};
+//again new game ke liye
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+//show the winner 
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulation, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+const checkWinner=()=>{
+    for(let pattern of winPatterns){// esse pattern nikelega like:0,1,2; 0,3,6;
+        console.log(pattern);// output in array like:[0, 1, 2],[0, 3, 6];
+        console.log(pattern[0],pattern[1],pattern[2]);//individual index aab nikala hain aur position check in value like: 0,1,2; 0,3,6;
+        console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]])//html ka button acces hoga position ke sath
+
+        let posVal1=boxes[pattern[0]].innerText;
+        let posVal2=boxes[pattern[1]].innerText;
+        let posVal3=boxes[pattern[2]].innerText;
+
+        if(posVal1!=="" && posVal2!=="" && posVal3!==""){
+            if(posVal1===posVal2 && posVal2===posVal3){
+                showWinner(posVal1);//who are winner calling
+            }
+        }
+
+
+    }
+}
+newGameBtn.addEventListener('click',resetGame);
+resetBtn.addEventListener('click',resetGame);
+
+
+
